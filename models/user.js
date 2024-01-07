7baf33733c9e9c755d747c371abee058cab55091
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-01-07 20:57:13 +0530

    hashed the user password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require('mongoose')
+
+
+// hashing the password 
+const { createHmac, randomBytes } = require('node:crypto')
 
 const userSchema = new Schema({
     username: {
@@ -27,9 +31,53 @@ const userSchema = new Schema({
         enum: ["USER", "ADMIN"],
         default: "USER",
     }
-    
-}, { timestamps: true});
 
-const User = model('user',userSchema);
+}, { timestamps: true });
+
+
+// hashing password
+userSchema.pre("save", function (next) {
+    const user = this;
+
+    if (!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+
+    const hashedPassword = createHmac("sha256", salt)
+        .update(user.password)
+        .digest("hex");
+    
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+})
+
+// Virtual Function for matching the hash password
+
+userSchema.static("matchPassword", async function (email, password) {
+    const user = await this.findOne({ email });
+    if (!user) throw new Error("User not found!");
+
+    // User password store in db
+    const salt = user.salt;
+    const hashedPassword = user.password;
+    //  *** //
+
+    // User provided password for login 
+    const userProvidedHash = createHmac("sha256", salt)
+        .update(password)
+        .digest("hex");
+    //  ****** //
+
+    if (hashedPassword !== userProvidedHash)
+        throw new Error("Incorrect Password!")
+
+    // if match password then it will only return user
+    return user;
+
+})
+//  ****** //
+const User = model('user', userSchema);
 
 module.exports = User;
