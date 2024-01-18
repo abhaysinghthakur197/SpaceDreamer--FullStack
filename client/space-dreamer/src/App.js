bb6a5d6e6dd2bb592f6845799011ef57f9da3055
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-01-19 01:01:38 +0530

    add router,route,routes

--- client/space-dreamer/src/App.js
+++ client/space-dreamer/src/App.js
@@ -2,13 +2,27 @@
 import './App.css';
 import Navbar from './component/Navbar/Navbar'
 import Welcome from './component/Home/Welcome'
+import News from './component/Home/SpaceNews/News'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Addnews from './component/Addnews/Addnews';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-     <Navbar />
-     <Welcome />
-    </div>
+    <Router>
+      <div className="bg-black">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Welcome/>}>
+          </Route>
+          <Route path='/news' element={<News/>}>
+          </Route>
+          <Route path='/addnews' element={ <Addnews/>}>
+           
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
