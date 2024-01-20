fe724f81e77715cede6d9ca81901e851b73226c7
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-01-20 16:16:48 +0530

    updated addnews page

--- client/space-dreamer/src/component/Addnews/Addnews.jsx
+++ client/space-dreamer/src/component/Addnews/Addnews.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Articleform from './Articleform/Articleform';
+
+
 
 const Addnews = () => {
     return (
 
         <Container>
             <div>
-                <h1>Add news</h1>
-                <form >
-                <div class="mb-3">
-                    <label for="coverImage" class="form-label">Title: </label>
-                    <input type="file" class="form-control" id="coverImage" name="coverImage" aria-describedby="coverImage" />
-                </div>
-                <div class="mb-3">
-                    <label for="title" class="form-label">Title: </label>
-                    <input type="text" class="form-control" id="title" name="title" aria-describedby="title" />
-                </div>
+                <div>
+                    <Row>
+                        <Col lg={8} xm={12}>
+                            <img src={process.env.PUBLIC_URL + 'addArticleGif.gif'} alt="addArticleImg" className='img-fluid' width={2500} style={{height: '100vh', objectFit: 'cover' }} />
+                        </Col>
+                        <Col lg={4} >
+                            <h1 className='text-center text-white my-5'>Let'<span className='text-warning'>s</span>  Explore the <br></br><span style={{ fontWeight: 'bolder', fontFamily: 'Helvetica, Arial, sans-serif', color: '#834651' }}>Cosmos World</span></h1>
+                        </Col>
 
-                <div class="mb-3">
-                    <label for="body">Body: </label>
-                    <textarea name="body" id="body" class="form-control"></textarea>
-                </div>
+                    </Row>
 
-                <div class="mb-3">
-                   <button class="btn btn-primary">Submit</button>
                 </div>
-                </form>
+                <div className="d-flex align-items-center justify-content-center mt-5">
+                    <Articleform/>
+                </div>
             </div>
-        </Container>
+            <div>
+                <h1>footer</h1>
+            </div>
+        </Container >
     )
 }
 
