dbeb6981b5a062c120f5433ad103f2aa186cc474
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-01-31 18:46:24 +0530

    added article route

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ const express = require('express')
 const connectToMongoDB = require('./connect')
 
 const userRoute = require('./routes/user')
+const articleRoute = require('./routes/article')
 
 // Adding cors for getting the req
 const cors = require('cors')
+
+// Adding cookie parser middleware
+const cookieParser  = require('cookie-parser')
 
 const app = express();
 const PORT = 8080
@@ -16,15 +20,17 @@ app.use(cors());
 connectToMongoDB("mongodb://127.0.0.1:27017/SpaceDreamer-app").then(() => console.log("Db connected")).catch((e) => console.log(e));
 
 // middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // app.use("/api", (req,res) => {
 //     console.log("server is ready")
 //     res.json("hello from server!")
 // })
 
-app.use("/api/user",userRoute)
+app.use("/api/user", userRoute)
+app.use("/api/article",articleRoute)
 
 
 
