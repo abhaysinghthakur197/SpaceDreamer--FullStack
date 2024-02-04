f7f8d87d185e4fc95c669debb64187adaa85a5e8
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-02-04 23:53:10 +0530

    save the article in db

--- server/routes/article.js
+++ server/routes/article.js
@@ -14,7 +14,7 @@ const path = require('path')
 //  uploading the file
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.resolve(`./public`));
+        cb(null, path.resolve(`./public/articleCoverImage`));
     },
     filename: function (req, file, cb) {
         const filename = `${Date.now()}-${file.originalname}`;
@@ -26,17 +26,25 @@ const upload = multer({ storage: storage });
 //****//
 
 
-router.post("/addnews", upload.single("coverImage"), (req, res) => {
+router.post("/addnews", upload.single("coverImage"), async (req, res) => {    
+    console.log(req.body)
+    console.log(req.file)
     if (req.fileValidationError) {
         return res.status(400).json({ error: req.fileValidationError });
     }
     if (!req.file) {
         return res.status(402).json({ error: 'No file received' });
     }
+    
+    const {title, body} = req.body;
+    const article = await Article.create({
+        title,
+        body,
+        // createdBy: req.user._id,
+        coverImageURL: `./public/articleCoverImage/${req.file.filename}`,
+    })
 
-    console.log(req.body);
-    console.log(req.file)
-    return res.json({ message: 'done news' });
+    return res.json({ message: 'uploaded news' });
 })
 
 module.exports = router;
