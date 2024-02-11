43c680bc1812fe693c8b4ac8ebf3f521347e7d53
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2024-02-11 23:59:59 +0530

    edited all card

--- client/space-dreamer/src/component/Home/SpaceNews/SpaceCard.jsx
+++ client/space-dreamer/src/component/Home/SpaceNews/SpaceCard.jsx
@@ -1,6 +1,6 @@
 import moment from "moment";
 import { Link } from "react-router-dom";
-import { Card, Row, Col, Container } from 'react-bootstrap'
+import { Card, Row, Col, Container, Button } from 'react-bootstrap'
 import '../../../../src/index.css'
 import './SpaceCard.css'
 
@@ -9,18 +9,20 @@ const SpaceCard = ({ news }) => {
     console.log("NEWS", news);
 
     return (
-        <Container>
-            <p className="text-center text-dark news_box_p">SPACE NEWS BOX</p>
+        <Container className="py-5">
+
             <Row xs={1} md={2} lg={3}>
                 {news.map((item, index) => (
-                    <Col key={index} className="px-4 py-y" >
-                        <Card className="h-100 card  rounded" > 
-                            <Card.Img variant="top" src={item.imageUrl} alt={item.altText} className="img-fluid" />
+                    <Col key={index} className="px-4 py-y mb-5 ml-2">
+                        <Card style={{ width: "25rem" }} className="border-warning border-2 bg-dark" >
+                            <Card.Img variant="top" src={item.imageUrl} alt={item.altText} className="p-1" style={{ borderRadius: '15px' }} />
                             <Card.Body>
-                                <Card.Title>{item.title}</Card.Title>
-                                <Card.Text className="card-description">{truncateDescription(item.summary)}</Card.Text>
-                                <Card.Text>{moment(item.publishedAt).format("dddd, MMMM Do, YYYY")}</Card.Text>
-                                    <button variant="danger">{item.newsSite}</button>
+                                <Card.Title className="text-white">{item.title}</Card.Title>
+                                <Card.Text className="text-white">{moment(item.publishedAt).format("dddd, MMMM Do, YYYY")}</Card.Text>
+                                <Card.Text className="card-description text-white">{truncateDescription(item.summary)}</Card.Text>
+                                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                    <Button variant='outline-light'>{item.newsSite}</Button>
+                                </a>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -34,6 +36,6 @@ const truncateDescription = (description) => {
     // Truncate the description to a fixed number of lines (e.g., 3 lines)
     const lines = description.split('\n').slice(0, 3);
     return lines.join('\n');
-  };
+};
 
 export default SpaceCard;
